7c7c8bb735e99f1cf29737fe6dc43aaa10882ec6
dkutelov <dkutelov@digitals.bg>
2022-02-26 18:09:11 +0200

    Checkout form

--- amazon/src/screens/AddressScreen/index.tsx
+++ amazon/src/screens/AddressScreen/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import {View, Text, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Alert} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import {styles} from './styles';
 import counitryList from 'country-list';
+import {Button} from '../../components/Button';
 
 const countries = counitryList.getData();
 interface Country {
@@ -10,11 +11,50 @@ interface Country {
   name: string;
 }
 
+interface FormValues {
+  fullname: string;
+  phoneNumber: string;
+  address: string;
+}
+
 export const AddressScreen = () => {
-  const [country, setCountry] = React.useState(countries[0].code);
+  const [country, setCountry] = useState(countries[0].code);
+  const [formValues, setFormValues] = useState<FormValues>({
+    fullname: '',
+    phoneNumber: '',
+    address: '',
+  });
+
+  const [formErrorValues, setFormErrorValues] = useState({
+    fullnameError: '',
+  });
+
+  const fullNameValid = (fullname: string) => {
+    const names = fullname.split(' ');
+    return names.length >= 2;
+  };
+
+  const onCheckout = () => {
+    if (
+      !formValues.fullname ||
+      !formValues.phoneNumber ||
+      !formValues.address
+    ) {
+      Alert.alert('Please, fill in all required fields!');
+    }
+
+    if (!fullNameValid(formValues.fullname)) {
+      setFormErrorValues({
+        ...formErrorValues,
+        fullnameError: 'Full name should contain at least two names!',
+      });
+    }
+  };
+  console.warn(formValues);
   return (
-    <View>
+    <View style={styles.root}>
       <View style={styles.row}>
+        <Text style={styles.label}>Select Your Country</Text>
         <Picker selectedValue={country} onValueChange={setCountry}>
           {countries.map((c: Country) => (
             <Picker.Item value={c.code} label={c.name} key={c.code} />
@@ -23,8 +63,46 @@ export const AddressScreen = () => {
       </View>
       <View style={styles.row}>
         <Text style={styles.label}>Full Name (First and Last name)</Text>
-        <TextInput />
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          value={formValues.fullname}
+          onChangeText={fullname => {
+            setFormValues({...formValues, fullname});
+            setFormErrorValues({
+              ...formErrorValues,
+              fullnameError: '',
+            });
+          }}
+        />
+        {formErrorValues.fullnameError.length > 0 && (
+          <Text style={styles.error}>{formErrorValues.fullnameError}</Text>
+        )}
       </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Phone Number</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Phone Number"
+          value={formValues.phoneNumber}
+          onChangeText={phoneNumber => {
+            setFormValues({...formValues, phoneNumber});
+          }}
+          keyboardType={'phone-pad'}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Address</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Your Address"
+          value={formValues.address}
+          onChangeText={address => {
+            setFormValues({...formValues, address});
+          }}
+        />
+      </View>
+      <Button text="Check Out" onPress={onCheckout} />
     </View>
   );
 };
--- amazon/src/screens/AddressScreen/styles.ts
+++ amazon/src/screens/AddressScreen/styles.ts
@@ -1,6 +1,28 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  row: {},
-  label: {},
+  root: {
+    marginTop: 20,
+    padding: 10,
+    height: '100%',
+  },
+  row: {
+    marginVertical: 5,
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    backgroundColor: 'white',
+    padding: 5,
+    marginVertical: 5,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 2,
+  },
+  error: {
+    color: 'red',
+    fontSize: 12,
+  },
 });
