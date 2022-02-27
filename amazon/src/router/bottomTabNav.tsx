d337fea7c7cc4996cf036bcbdb59fbccc4007eea
dkutelov <dkutelov@digitals.bg>
2022-02-27 15:36:39 +0200

    tab bar icons and styling

--- amazon/src/router/bottomTabNav.tsx
+++ amazon/src/router/bottomTabNav.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeScreen} from '../screens/HomeScreen';
 import ShoppingCartScreen from '../screens/ShoppingCartScreen';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,11 +11,45 @@ export const BottomTabNav = () => {
     <Tab.Navigator
       screenOptions={{
         tabBarShowLabel: false,
+        tabBarInactiveTintColor: '#c9c9c9',
+        tabBarActiveTintColor: '#e47911',
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={HomeScreen} />
-      <Tab.Screen name="Cart" component={ShoppingCartScreen} />
-      <Tab.Screen name="More" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}: {color: string}) => (
+            <Entypo name="home" color={color} size={25} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}: {color: string}) => (
+            <Entypo name="user" color={color} size={25} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={ShoppingCartScreen}
+        options={{
+          tabBarIcon: ({color}: {color: string}) => (
+            <Entypo name="shopping-cart" color={color} size={25} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="More"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}: {color: string}) => (
+            <Entypo name="menu" color={color} size={25} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
