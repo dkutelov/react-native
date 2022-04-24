1f5a48de25f0ace1023af920b369ac88f1b57d2d
dkutelov <dkutelov@digitals.bg>
2022-04-24 20:21:36 +0300

    Tab navigation setup

--- uber/UberEeatsUser/src/navigation/index.js
+++ uber/UberEeatsUser/src/navigation/index.js
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Foundation, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 import { HomeScreen } from "../screens/home";
 import { BasketScreen } from "../screens/basket";
@@ -21,3 +23,23 @@ export const RootNavigator = () => {
     </Stack.Navigator>
   );
 };
+
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeTabs = () => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Foundation name="home" size={24} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name="Orders" component={OrderDetails} />
+    <Tab.Screen name="Profile" component={OrderDetails} />
+  </Tab.Navigator>
+);
+
+export default HomeTabs;
