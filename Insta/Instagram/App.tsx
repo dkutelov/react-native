93bf67f90d96129c5cf1e6ea25339ba0c39fedc5
dkutelov <dkutelov@digitals.bg>
2022-06-26 22:17:30 +0300

    icons set up, post UI

--- Insta/Instagram/App.tsx
+++ Insta/Instagram/App.tsx
@@ -1,16 +1,145 @@
-// import React from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 
 const App = () => {
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text style={{color: colors.primary, fontSize: fonts.size.xlg}}>
-        Hello World
-      </Text>
+    <View style={styles.post}>
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>DariyKutelov</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        style={styles.image}
+      />
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>someuser</Text> and{' '}
+          <Text style={styles.bold}>66</Text> others
+        </Text>
+        <Text style={styles.text}>
+          <Text style={styles.bold}>dariykutelov</Text> Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Dignissimos autem iste rerum rem
+          quis quas, magnam laudantium ratione aperiam voluptas neque atque
+          excepturi, doloremque pariatur repellendus modi sint quos eaque!
+        </Text>
+
+        {/* Comment */}
+        <Text>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>dariykutelov</Text> Lorem ipsum dolor sit
+            amet consectetur adipisicing elit.
+          </Text>
+          <AntDesign
+            name={'hearto'}
+            size={14}
+            style={styles.icon}
+            color={colors.black}
+          />
+        </View>
+        {/* Posted date */}
+        <Text>19 December, 2021</Text>
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  post: {
+    marginTop: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  footer: {
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  icon: {
+    marginHorizontal: 5,
+  },
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  bold: {
+    fontWeight: fonts.weight.bold,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  commentText: {
+    color: colors.black,
+    lineHeight: 18,
+    flex: 1,
+  },
+});
 
 export default App;
